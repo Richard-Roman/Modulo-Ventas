8ccf4f06e1195ec98eddb5cf98eb0abe0d8b0388
Franky0512 <fj.villoslada@unsm.edu.pe>
2024-05-30 11:05:04 -0500

    gestio vendedor
    agregado gestion vendedor

--- src/vendedor.java
+++ src/vendedor.java
@@ -12,4 +12,31 @@ public class vendedor extends persona{
    public void imprimir(){
       System.out.println(this.toString());
    }
+
+   public boolean esIgualVendedor(vendedor b){
+      persona c = b;  
+      if(this.getId() == (b.getId())){
+         return super.esIgual(c);
+      } else {
+         return false;
+      }
+   }
+
+
+   public static vendedor crearVendedor(){
+      System.out.println("Ingrese los siguientes datos:");
+      int id = leer.Entero("DNI ");
+      vendedor nuevo = new vendedor(id); 
+      return nuevo;
+   }
+
+   public static vendedor completarRegistrovVendedor(vendedor auxi){
+      auxi.setNombre(leer.Cadena("Nombre"));
+      auxi.setTelefono(leer.Entero("Telefono"));
+
+   return auxi;
+}
+
+
+   
 }
